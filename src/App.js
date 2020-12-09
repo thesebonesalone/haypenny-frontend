0a7d3981fd910d2bf52af6d121653ebe777c8fd6
thesebonesalone <duikhead2@gmail.com>
2020-12-09 13:20:50 -0600

    Setting up React-Router-Dom and Redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import URL from "./Constants/URL";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const reducer = (state = { user: { name: "", id: null } }, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.user };
+    case "LOGOUT":
+      return { ...state, user: { name: "", id: null } };
+  }
+};
+const store = createStore(
+  reducer,
+  [] + window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+store.dispatch({ type: "@@INIT" });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Provider store={store}>
+        <div>THIS IS WORKING HOORAY</div>
+      </Provider>
+    </Router>
   );
 }
 
---
+++ src/Constants/URL.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default URL = "http://localhost:3001"
